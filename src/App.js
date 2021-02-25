7e21f966fe2bf11a5342d2d1bfebcf2a07bcc478
Xin Liu <liu.xin7@northeastern.edu>
2021-02-24 18:37:34 -0800

    Added App js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./components/home";
+import CourseManager from "./components/course-manager";
+import CourseEditor from "./components/course-editor";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Route path="/" exact={true} component={Home}/>
+          <Route path="/courses" component={CourseManager}/>
+          <Route path="/courses/editor" render={(props) => <CourseEditor {...props}/>}/>
+      </BrowserRouter>
+
   );
 }
 
