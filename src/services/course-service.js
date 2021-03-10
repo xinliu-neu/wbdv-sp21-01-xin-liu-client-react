6dad38b4bdae9b0b27738dfa708c77313a44fd5e
Xin Liu <liu.xin7@northeastern.edu>
2021-03-10 14:11:40 -0800

    Formatted code

--- src/services/course-service.js
+++ src/services/course-service.js
@@ -1,39 +1,38 @@
-const COURSES_URL = "https://wbdv-generic-server.herokuapp.com/api/001797605/courses"
+export const COURSES_URL = "https://wbdv-generic-server.herokuapp.com/api/001797605/courses"
 
 export const findAllCourses = () =>
     fetch(COURSES_URL)
-      .then(response => response.json())
+    .then(response => response.json())
 
 export const deleteCourse = (courseId) =>
-    fetch( `${COURSES_URL}/${courseId}`, {
-        method:'DELETE'
+    fetch(`${COURSES_URL}/${courseId}`, {
+      method: 'DELETE'
     })
-       .then(response => response.json())
+    .then(response => response.json())
 
 export const createCourse = (course) =>
-    fetch( COURSES_URL, {
-        method:'POST',
-        body: JSON.stringify(course),
-        headers:{
-          'content-type': 'application/json'
-        }
+    fetch(COURSES_URL, {
+      method: 'POST',
+      body: JSON.stringify(course),
+      headers: {
+        'content-type': 'application/json'
+      }
     })
-      .then(response => response.json())
+    .then(response => response.json())
 
 export const updateCourse = (courseId, course) =>
     fetch(`${COURSES_URL}/${courseId}`, {
-        method: 'PUT',
-        body: JSON.stringify(course),
-        headers: {
-            'content-type': 'application/json'
-        }
+      method: 'PUT',
+      body: JSON.stringify(course),
+      headers: {
+        'content-type': 'application/json'
+      }
     })
-        .then(response => response.json())
+    .then(response => response.json())
 
 export const findCourseById = (id) =>
     fetch(`${COURSES_URL}/${id}`)
-        .then(response => response.json())
-
+    .then(response => response.json())
 
 const api = {
   findAllCourses: findAllCourses,
