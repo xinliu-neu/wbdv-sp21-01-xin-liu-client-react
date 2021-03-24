add0abf8f69db39dae629b5ce37084ef07d5d3c2
Xin Liu <liu.xin7@northeastern.edu>
2021-03-23 21:12:23 -0700

    Added Paragraph Widget component

---
+++ src/components/widgets/paragraph-widget.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+
+const ParagraphWidget = ({widget, setEditingWidget, editing}) => {
+  const [localWidget, setLocalWidget] = useState(widget);
+
+  return (
+      <>
+        {
+          editing &&
+          <>
+            <select onChange={e => {
+              setLocalWidget({
+                ...localWidget,
+                type: e.target.value
+              });
+              setEditingWidget({
+                ...localWidget,
+                type: e.target.value
+              });
+            }}
+                    value={localWidget.type} className="form-control">
+              <option value="HEADING">Heading</option>
+              <option value="PARAGRAPH">Paragraph</option>
+            </select>
+
+            <br/>
+
+            <textarea onChange={e => {
+              setLocalWidget({
+                ...widget,
+                text: e.target.value
+              });
+              setEditingWidget({
+                ...widget,
+                text: e.target.value
+              });
+            }}
+                      value={localWidget.text} className="form-control"/>
+          </>
+        }
+        {
+          !editing &&
+          <p>
+            {localWidget.text}
+          </p>
+        }
+      </>
+  )
+}
+
+export default ParagraphWidget;
