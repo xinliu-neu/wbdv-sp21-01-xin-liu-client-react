629004db3cee2479efa334747e65b067dab64fc9
Xin Liu <liu.xin7@northeastern.edu>
2021-04-19 20:53:21 -0700

    Added service for attempts

---
+++ src/services/attempt-service.js
@@ -0,0 +1,10 @@
+const QUIZZES_URL = "http://localhost:3000/api/quizzes"
+
+const findAttemptsForQuiz = (quizId) => {
+  return fetch(`${QUIZZES_URL}/${quizId}/attempts`)
+  .then(response => response.json());
+}
+
+export default {
+  findAttemptsForQuiz
+};
